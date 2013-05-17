5135817f5e76ac58c52df3b8a0744ac70a765755
Charles Rosaaen <charlesrosaaen@gmail.com>
2013-05-16 23:28:09 -0700

    changed to hashmap stored in helper

--- src/com/ioabsoftware/DroidFAQs/db/HighlightListDBHelper.java
+++ src/com/ioabsoftware/DroidFAQs/db/HighlightListDBHelper.java
@@ -1,6 +1,8 @@
 package com.ioabsoftware.DroidFAQs.db;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Locale;
 
 import android.content.ContentValues;
 import android.content.Context;
@@ -9,6 +11,14 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class HighlightListDBHelper extends SQLiteOpenHelper {
+	
+	private HashMap<String, HighlightedUser> highlightedUsers;
+	/**
+	 * DO NOT STORE REFERENCES TO THIS LIST! Always call this method when needing to use the list,
+	 * this list will always be up to date.
+	 * @return An up-to-date list of highlighted users.
+	 */
+	public HashMap<String, HighlightedUser> getHighlightedUsers() {return highlightedUsers;}
 	
 	private static final String DATABASE_NAME = "highlightlists.db";
 	private static final int DATABASE_VERSION = 1;
@@ -21,13 +31,17 @@ public class HighlightListDBHelper extends SQLiteOpenHelper {
 	
 	private static final String CREATE_TABLE_USERS = 
 			"create table " + TABLE_USERS + "(" + 
-			COLUMN_USERS_ID + " integer primary key autoincrement " +
-			COLUMN_USERS_NAME + " text primary key, " + 
+			COLUMN_USERS_ID + " integer primary key autoincrement, " +
+			COLUMN_USERS_NAME + " text not null, " + 
 			COLUMN_USERS_LABEL + " text not null, " +
 			COLUMN_USERS_COLOR + " integer not null);";
 
 	public HighlightListDBHelper(Context context) {
-	  super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+
+		SQLiteDatabase db = getWritableDatabase();
+		updateUsers(db);
+		db.close();
 	}
 	
 	@Override
@@ -41,7 +55,7 @@ public class HighlightListDBHelper extends SQLiteOpenHelper {
 
 	}
 	
-	public void addUser(String name, String label, int color) {
+	public HashMap<String, HighlightedUser> addUser(String name, String label, int color) {
 		SQLiteDatabase db = getWritableDatabase();
 		
 		ContentValues vals = new ContentValues();
@@ -50,40 +64,53 @@ public class HighlightListDBHelper extends SQLiteOpenHelper {
 		vals.put(COLUMN_USERS_COLOR, color);
 		
 		db.insert(TABLE_USERS, null, vals);
+		
+		updateUsers(db);
+		
 		db.close();
+		
+		return highlightedUsers;
 	}
 	
-	public void updateUser(String name, String label, int color) {
+	public HashMap<String, HighlightedUser> updateUser(HighlightedUser user) {
 		SQLiteDatabase db = getWritableDatabase();
 		
 		ContentValues vals = new ContentValues();
-		vals.put(COLUMN_USERS_NAME, name);
-		vals.put(COLUMN_USERS_LABEL, label);
-		vals.put(COLUMN_USERS_COLOR, color);
+		vals.put(COLUMN_USERS_NAME, user.getName());
+		vals.put(COLUMN_USERS_LABEL, user.getLabel());
+		vals.put(COLUMN_USERS_COLOR, user.getColor());
 		
-		db.update(TABLE_USERS, vals, COLUMN_USERS_NAME + " = " + name, null);
+		db.update(TABLE_USERS, vals, COLUMN_USERS_ID + " = " + user.getID(), null);
+		
+		updateUsers(db);
+		
 		db.close();
+		
+		return highlightedUsers;
 	}
 	
-	public void deleteUser(String name) {
+	public HashMap<String, HighlightedUser> deleteUser(HighlightedUser user) {
 		SQLiteDatabase db = getWritableDatabase();
-		db.delete(TABLE_USERS, COLUMN_USERS_NAME + " = " + name, null);
+		db.delete(TABLE_USERS, COLUMN_USERS_ID + " = " + user.getID(), null);
+		
+		updateUsers(db);
+		
 		db.close();
+		
+		return highlightedUsers;
 	}
 	
-//	public HashMap<String, String> getAllUsers() {
-//		SQLiteDatabase db = getWritableDatabase();
-//		HashMap<String, String> users = new HashMap<String, String>();
-//		
-//		Cursor cur = db.query(TABLE_USERS, null, null, null, null, null, null);
-//		
-//		if (cur.moveToFirst()) {
-//			do {
-//				users.put(cur.getString(0), cur.getString(1));
-//			} while (cur.moveToNext());
-//		}
-//		
-//		return users;
-//	}
+	private void updateUsers(SQLiteDatabase db) {
+		highlightedUsers = new HashMap<String, HighlightedUser>();
+		
+		Cursor cur = db.query(TABLE_USERS, null, null, null, null, null, null);
+		
+		if (cur.moveToFirst()) {
+			do {
+				highlightedUsers.put(cur.getString(1).toLowerCase(Locale.US), 
+						new HighlightedUser(cur.getInt(0), cur.getString(1), cur.getString(2), cur.getInt(3)));
+			} while (cur.moveToNext());
+		}
+	}
 
 }
