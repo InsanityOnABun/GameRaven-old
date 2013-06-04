a8090c3eb8cf753194bc5c691ebce27a32d55748
Charles Rosaaen <charlesrosaaen@gmail.com>
2013-06-03 21:15:04 -0700

    Revert "version gamma_build_13_01 release"
    This reverts commit 29ebc7fafb8794dac3e4189d030f030ecfc1ded2.

--- src/com/ioabsoftware/gameraven/db/HlUDDismissListener.java
+++
@@ -1,6 +0,0 @@
-package com.ioabsoftware.gameraven.db;
-
-public interface HlUDDismissListener {
-	
-	public void beforeDismissSuccessfulSave();
-}
--- src/com/ioabsoftware/gameraven/views/HighlightedUserView.java
+++ src/com/ioabsoftware/gameraven/views/HighlightedUserView.java
@@ -22,7 +22,6 @@ import android.widget.TextView;
 import com.ioabsoftware.gameraven.AllInOneV2;
 import com.ioabsoftware.gameraven.R;
 import com.ioabsoftware.gameraven.SettingsHighlightedUsers;
-import com.ioabsoftware.gameraven.db.HighlightListDBHelper;
 import com.ioabsoftware.gameraven.db.HighlightedUser;
 
 public class HighlightedUserView extends LinearLayout implements OnClickListener {
@@ -63,108 +62,118 @@ public class HighlightedUserView extends LinearLayout implements OnClickListener
 
 	@Override
 	public void onClick(View v) {
-		HighlightListDBHelper.showHighlightUserDialog(hlActivity, user, null, hlActivity);
-//		AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(hlActivity);
-//    	LayoutInflater inflater = hlActivity.getLayoutInflater();
-//    	final View dialogView = inflater.inflate(R.layout.highlightuserdialog, null);
-//    	dialogBuilder.setView(dialogView);
-//    	dialogBuilder.setTitle("Add Highlighted User");
-//    	
-//    	final EditText dName = (EditText) dialogView.findViewById(R.id.huName);
-//    	final EditText dLabel = (EditText) dialogView.findViewById(R.id.huLabel);
-//
-//    	final Button dSetColor = (Button) dialogView.findViewById(R.id.huSetColor);
-//    	final TextView dColorVal = (TextView) dialogView.findViewById(R.id.huColorVal);
-//    	
-//    	final CheckedTextView dDelete = (CheckedTextView) dialogView.findViewById(R.id.huDelete);
-//    	
-//    	dSetColor.setOnClickListener(new OnClickListener() {
-//			@Override
-//			public void onClick(View v) {
-//				int startColor = Integer.parseInt(dColorVal.getText().toString());
-//				
-//				ColorPickerDialog picker = new ColorPickerDialog(hlActivity, startColor);
-//				picker.setOnColorChangedListener(new OnColorChangedListener() {
-//					@Override
-//					public void onColorChanged(int color) {
-//						dSetColor.setBackgroundColor(color);
-//						dSetColor.setTextColor(~color | 0xFF000000); //without alpha
-//						dColorVal.setText(Integer.toString(color));
-//					}
-//				});
-//				picker.show();
-//			}
-//		});
-//    	
-//    	if (!isAddNew) {
-//    		dDelete.setVisibility(View.VISIBLE);
-//    		dDelete.setOnClickListener(new OnClickListener() {
-//				@Override
-//				public void onClick(View v) {
-//					((CheckedTextView) v).toggle();
-//				}
-//			});
-//    		
-//    		dName.setText(user.getName());
-//    		dLabel.setText(user.getLabel());
-//    		dSetColor.setBackgroundColor(user.getColor());
-//			dSetColor.setTextColor(~user.getColor() | 0xFF000000); //without alpha
-//    		dColorVal.setText(Integer.toString(user.getColor()));
-//    	}
-//    	
-//    	dialogBuilder.setPositiveButton("Save", null);
-//    	dialogBuilder.setNegativeButton("Cancel", null);
-//    	
-//    	
-//    	final AlertDialog diag = dialogBuilder.create();
-//    	diag.setOnShowListener(new OnShowListener() {
-//			@Override
-//			public void onShow(DialogInterface dialog) {
-//				diag.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
-//					@Override
-//					public void onClick(View v) {
-//						boolean shouldDismiss = true;
-//						
-//						if (!dDelete.isChecked()) {
-//							if (dName.getText().toString().length() > 0
-//									&& dLabel.getText().toString().length() > 0
-//									&& !dColorVal.getText().equals("0")) {
-//
-//								if (isAddNew) {
-//									AllInOneV2.getHLDB().addUser(
-//											dName.getText().toString(),
-//											dLabel.getText().toString(),
-//											Integer.parseInt(dColorVal
-//													.getText().toString()));
-//								} else {
-//									user.setName(dName.getText().toString());
-//									user.setLabel(dLabel.getText().toString());
-//									user.setColor(Integer.parseInt(dColorVal
-//											.getText().toString()));
-//									AllInOneV2.getHLDB().updateUser(user);
-//								}
-//
-//								
-//							} else {
-//								Toast.makeText(hlActivity,
-//										"Missing required info",
-//										Toast.LENGTH_SHORT).show();
-//								shouldDismiss = false;
+		AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(hlActivity);
+    	LayoutInflater inflater = hlActivity.getLayoutInflater();
+    	final View dialogView = inflater.inflate(R.layout.highlightuserdialog, null);
+    	dialogBuilder.setView(dialogView);
+    	dialogBuilder.setTitle("Add Highlighted User");
+    	
+    	final EditText dName = (EditText) dialogView.findViewById(R.id.huName);
+    	final EditText dLabel = (EditText) dialogView.findViewById(R.id.huLabel);
+
+    	final Button dSetColor = (Button) dialogView.findViewById(R.id.huSetColor);
+    	final TextView dColorVal = (TextView) dialogView.findViewById(R.id.huColorVal);
+    	
+    	final CheckedTextView dDelete = (CheckedTextView) dialogView.findViewById(R.id.huDelete);
+    	
+    	dSetColor.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				int startColor = Integer.parseInt(dColorVal.getText().toString());
+				
+				ColorPickerDialog picker = new ColorPickerDialog(hlActivity, startColor);
+				picker.setOnColorChangedListener(new OnColorChangedListener() {
+					@Override
+					public void onColorChanged(int color) {
+						dSetColor.setBackgroundColor(color);
+						dSetColor.setTextColor(~color | 0xFF000000); //without alpha
+						dColorVal.setText(Integer.toString(color));
+					}
+				});
+				picker.show();
+				
+//				new ColorSelectorDialog(hlActivity,
+//						new OnColorChangedListener() {
+//							@Override
+//							public void colorChanged(int newColor) {
+//								dSetColor.setBackgroundColor(newColor);
+//								dSetColor.setTextColor(~newColor | 0xFF000000); //without alpha
+//								dColorVal.setText(Integer.toString(newColor));
 //							}
-//						}
-//						else {
-//							AllInOneV2.getHLDB().deleteUser(user);
-//						}
-//						
-//						if (shouldDismiss) {
-//							hlActivity.updateList();
-//							diag.dismiss();
-//						}
-//					}
-//				});
-//			}
-//		});
-//    	diag.show();
+//						}, 
+//						startColor).show();
+			}
+		});
+    	
+    	if (!isAddNew) {
+    		dDelete.setVisibility(View.VISIBLE);
+    		dDelete.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					((CheckedTextView) v).toggle();
+				}
+			});
+    		
+    		dName.setText(user.getName());
+    		dLabel.setText(user.getLabel());
+    		dSetColor.setBackgroundColor(user.getColor());
+			dSetColor.setTextColor(~user.getColor() | 0xFF000000); //without alpha
+    		dColorVal.setText(Integer.toString(user.getColor()));
+    	}
+    	
+    	dialogBuilder.setPositiveButton("Save", null);
+    	dialogBuilder.setNegativeButton("Cancel", null);
+    	
+    	
+    	final AlertDialog diag = dialogBuilder.create();
+    	diag.setOnShowListener(new OnShowListener() {
+			@Override
+			public void onShow(DialogInterface dialog) {
+				diag.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						boolean shouldDismiss = true;
+						
+						if (!dDelete.isChecked()) {
+							if (dName.getText().toString().length() > 0
+									&& dLabel.getText().toString().length() > 0
+									&& !dColorVal.getText().equals("0")) {
+
+								if (isAddNew) {
+									AllInOneV2.getHLDB().addUser(
+											dName.getText().toString(),
+											dLabel.getText().toString(),
+											Integer.parseInt(dColorVal
+													.getText().toString()));
+								} else {
+									user.setName(dName.getText().toString());
+									user.setLabel(dLabel.getText().toString());
+									user.setColor(Integer.parseInt(dColorVal
+											.getText().toString()));
+									AllInOneV2.getHLDB().updateUser(user);
+								}
+
+								
+							} else {
+								Toast.makeText(hlActivity,
+										"Missing required info",
+										Toast.LENGTH_SHORT).show();
+								shouldDismiss = false;
+							}
+						}
+						else {
+							AllInOneV2.getHLDB().deleteUser(user);
+						}
+						
+						if (shouldDismiss) {
+							hlActivity.updateList();
+							diag.dismiss();
+						}
+					}
+				});
+			}
+		});
+    	diag.show();
 	}
 	
 	
