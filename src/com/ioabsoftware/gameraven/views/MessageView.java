6a3a33e057e5aae97fb6140a44e79d64ce08f621
Charles Rosaaen <charlesrosaaen@gmail.com>
2013-06-10 03:00:14 -0700

    work on custom messageview parser

--- src/com/ioabsoftware/gameraven/views/MessageView.java
+++ src/com/ioabsoftware/gameraven/views/MessageView.java
@@ -9,23 +9,25 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Typeface;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.StateListDrawable;
-import android.graphics.drawable.shapes.Shape;
-import android.text.Html;
+import android.os.Parcel;
+import android.text.Layout;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
-import android.text.Spanned;
 import android.text.TextPaint;
-import android.text.method.LinkMovementMethod;
-import android.text.method.MovementMethod;
 import android.text.style.BackgroundColorSpan;
+import android.text.style.CharacterStyle;
 import android.text.style.ClickableSpan;
+import android.text.style.QuoteSpan;
 import android.text.style.StyleSpan;
+import android.text.style.TypefaceSpan;
+import android.text.style.UnderlineSpan;
 import android.util.StateSet;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -36,15 +38,16 @@ import android.widget.TextView;
 
 import com.ioabsoftware.gameraven.AllInOneV2;
 import com.ioabsoftware.gameraven.R;
-import com.ioabsoftware.gameraven.URLImageParser;
-import com.ioabsoftware.gameraven.networking.Session;
 import com.ioabsoftware.gameraven.networking.HandlesNetworkResult.NetDesc;
+import com.ioabsoftware.gameraven.networking.Session;
 
 public class MessageView extends LinearLayout implements View.OnClickListener {
 
 	private String userContent, messageID, boardID, topicID;
-	private Element messageContent;
+	private Element messageContent, messageContentNoPoll;
 	private AllInOneV2 aio;
+	
+	private static int quoteBackColor = Color.argb(255, 100, 100, 100);
 	
 	public String getUser() {
 		return userContent;
@@ -103,13 +106,14 @@ public class MessageView extends LinearLayout implements View.OnClickListener {
         String html = null;
         if (messageContent.getElementsByClass("board_poll").isEmpty()) {
     		aio.wtl("no poll");
-        	html = messageContent.html();
+    		messageContentNoPoll = messageContent.clone();
+        	html = messageContentNoPoll.html();
 		}
         else {
     		aio.wtl("there is a poll");
-        	Element ed = messageContent.clone();
-        	ed.getElementsByClass("board_poll").first().remove();
-        	html = ed.html();
+    		messageContentNoPoll = messageContent.clone();
+    		messageContentNoPoll.getElementsByClass("board_poll").first().remove();
+        	html = messageContentNoPoll.html();
 
     		LinearLayout pollWrapper = (LinearLayout) findViewById(R.id.mvPollWrapper);
     		LinearLayout innerPollWrapper = new LinearLayout(aio);
@@ -189,20 +193,42 @@ public class MessageView extends LinearLayout implements View.OnClickListener {
 //        message.setText(Html.fromHtml(html, null, null));
         
         SpannableStringBuilder ssb = new SpannableStringBuilder(processContent(false));
-        while (ssb.toString().contains("<b>")) {
-        	int start = ssb.toString().indexOf("<b>");
-        	ssb.delete(start, start + 3);
-        	int end = ssb.toString().indexOf("</b>", start);
-        	ssb.delete(end, end + 4);
-        	ssb.setSpan(new StyleSpan(Typeface.BOLD), start, end, 0);
+        
+        addSpan(ssb, "<b>", "</b>", new StyleSpan(Typeface.BOLD));
+        addSpan(ssb, "<i>", "</i>", new StyleSpan(Typeface.ITALIC));
+        addSpan(ssb, "<code>", "</code>", new TypefaceSpan("monospace"));
+        addSpan(ssb, "<cite>", "</cite>", new UnderlineSpan());
+        
+        // quotes don't use CharacterStyles, so do it manually
+        while (ssb.toString().contains("<blockquote>")) {
+        	int start = ssb.toString().indexOf("<blockquote>");
+        	ssb.delete(start, start + "<blockquote>".length());
+        	
+        	int stackCount = 1;
+        	int closer;
+        	int opener;
+        	int innerStartPoint = start;
+        	do {
+        		opener = ssb.toString().indexOf("<blockquote>", innerStartPoint + "<blockquote>".length());
+        		closer = ssb.toString().indexOf("</blockquote>", innerStartPoint + "<blockquote>".length());
+        		if (opener != -1 && opener < closer) {
+        			// found a nested quote
+        			stackCount++;
+        			innerStartPoint = opener;
+        		}
+        		else {
+        			// this closer is the right one
+        			stackCount--;
+        			innerStartPoint = closer;
+        		}
+        	} while (stackCount > 0);
+        	
+        	
+        	ssb.delete(closer, closer + "</blockquote>".length());
+        	aio.wtl("quote being added to post " + postNum + ": " + ssb.subSequence(start, closer));
+        	ssb.setSpan(new GRQuoteSpan(), start, closer, 0);
         }
-        while (ssb.toString().contains("<i>")) {
-        	int start = ssb.toString().indexOf("<i>");
-        	ssb.delete(start, start + 3);
-        	int end = ssb.toString().indexOf("</i>", start);
-        	ssb.delete(end, end + 4);
-        	ssb.setSpan(new StyleSpan(Typeface.ITALIC), start, end, 0);
-        }
+        
         
         final int defTextColor = message.getTextColors().getDefaultColor();
         final int color;
@@ -210,6 +236,9 @@ public class MessageView extends LinearLayout implements View.OnClickListener {
 			color = Color.WHITE;
 		else
 			color = Color.BLACK;
+		
+		// do spoiler tags manually instead of in the method, as the clickablespan needs
+		// to know the start and end points
         while (ssb.toString().contains("<spoiler>")) {
         	final int start = ssb.toString().indexOf("<spoiler>");
         	ssb.delete(start, start + 9);
@@ -232,6 +261,7 @@ public class MessageView extends LinearLayout implements View.OnClickListener {
         }
         
         message.setText(ssb);
+        
         
         message.setLinkTextColor(AllInOneV2.getAccentColor());
 
@@ -273,6 +303,17 @@ public class MessageView extends LinearLayout implements View.OnClickListener {
 
 		aio.wtl("finishing mv creation");
 	}
+	
+	private void addSpan(SpannableStringBuilder ssb, String tag, String endTag, CharacterStyle... cs) {
+		while (ssb.toString().contains(tag)) {
+        	int start = ssb.toString().indexOf(tag);
+        	ssb.delete(start, start + tag.length());
+        	int end = ssb.toString().indexOf(endTag, start);
+        	ssb.delete(end, end + endTag.length());
+        	for (CharacterStyle c : cs)
+        		ssb.setSpan(CharacterStyle.wrap(c), start, end, 0);
+        }
+	}
 
 	@Override
 	public void onClick(View v) {
@@ -288,18 +329,7 @@ public class MessageView extends LinearLayout implements View.OnClickListener {
 	}
 
 	private String processContent(boolean removeSig) {
-		Element clonedBody = messageContent.getElementsByClass("msg_body").first().clone();
-		if (!clonedBody.getElementsByClass("board_poll").isEmpty()) {
-			clonedBody.getElementsByClass("board_poll").first().remove();
-		}
-
-		String finalBody = clonedBody.html();
-
-		if (removeSig) {
-			int sigStart = finalBody.lastIndexOf("---");
-			if (sigStart != -1)
-				finalBody = finalBody.substring(0, sigStart);
-		}
+		String finalBody = messageContentNoPoll.getElementsByClass("msg_body").first().html();
 		
 		finalBody = finalBody.replace("<span class=\"fspoiler\">", "<spoiler>").replace("</span>", "</spoiler>");
 		
@@ -316,8 +346,53 @@ public class MessageView extends LinearLayout implements View.OnClickListener {
 			finalBody = finalBody.substring(0, finalBody.length() - 6);
 		finalBody = finalBody.replace("\n", "");
 		finalBody = finalBody.replace("<br />", "\n");
+
+		if (removeSig) {
+			int sigStart = finalBody.lastIndexOf("\n---\n");
+			if (sigStart != -1)
+				finalBody = finalBody.substring(0, sigStart);
+		}
 		
 		finalBody = StringEscapeUtils.unescapeHtml4(finalBody);
 		return finalBody;
+	}
+	
+	
+	
+	private class GRQuoteSpan extends QuoteSpan {
+		
+		private static final int WIDTH = 4;
+		private static final int GAP = 4;
+		private final int COLOR = AllInOneV2.getAccentColor();
+		
+		public void writeToParcel(Parcel dest, int flags) {
+	        dest.writeInt(COLOR);
+	    }
+
+	    public int getColor() {
+	        return COLOR;
+	    }
+	    
+	    public int getLeadingMargin(boolean first) {
+	        return WIDTH + GAP;
+	    }
+		
+		@Override
+		public void drawLeadingMargin(Canvas c, Paint p, int x, int dir,
+                int top, int baseline, int bottom,
+                CharSequence text, int start, int end,
+                boolean first, Layout layout) {
+			Paint.Style style = p.getStyle();
+			int color = p.getColor();
+
+			p.setStyle(Paint.Style.FILL);
+			p.setColor(COLOR);
+
+			c.drawRect(x, top, x + dir * WIDTH, bottom, p);
+			
+			p.setStyle(style);
+			p.setColor(color);
+		}
+		
 	}
 }
