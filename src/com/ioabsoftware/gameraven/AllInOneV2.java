2f5ac6b3263161603977fb65b7c7a22e7da41794
InsanityOnABun <charlesrosaaen@gmail.com>
2014-01-21 05:07:19 -0600

    Import cleanup

--- src/com/ioabsoftware/gameraven/AllInOneV2.java
+++ src/com/ioabsoftware/gameraven/AllInOneV2.java
@@ -63,7 +63,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewGroup.LayoutParams;
 import android.view.inputmethod.InputMethodManager;
 import android.webkit.WebView;
 import android.widget.AdapterView;
