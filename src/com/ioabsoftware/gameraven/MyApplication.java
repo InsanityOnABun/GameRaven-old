75bab97e7aeaade01041cbce554f49c3c49b30db
InsanityOnABun <charlesrosaaen@gmail.com>
2014-02-18 04:43:44 -0600

    ACRA work

--- src/com/ioabsoftware/gameraven/MyApplication.java
+++ src/com/ioabsoftware/gameraven/MyApplication.java
@@ -5,7 +5,14 @@ import org.acra.annotation.ReportsCrashes;
 
 import android.app.Application;
 
-@ReportsCrashes(formKey = "", formUri = "")
+@ReportsCrashes(
+		formKey = "",
+		formUri = "https://ioabsoftware.cloudant.com/acra-gameraven/_design/acra-storage/_update/report",
+        reportType = org.acra.sender.HttpSender.Type.JSON,
+        httpMethod = org.acra.sender.HttpSender.Method.PUT,
+        formUriBasicAuthLogin="somedfoldersessesetteade",
+        formUriBasicAuthPassword="Kg4y3nMrROogBSvqV87ASVJM"
+		)
 public class MyApplication extends Application {
 	@Override
     public void onCreate() {
